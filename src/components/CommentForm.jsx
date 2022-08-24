1ebea5297fc86a85f86f0ffa0dfff4104fba78ab
Alex-J-Jackson <alexander-jackson@outlook.com>
2022-08-24 11:21:32 +0100

    add main delete functionality

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -4,10 +4,10 @@ import { useContext, useState } from "react";
 import { UserContext } from "../contexts/User";
 import { postComment } from "../api";
 
-const CommentForm = ({ review_id }) => {
+const CommentForm = ({ review_id, posted, setPosted }) => {
   const { user } = useContext(UserContext);
   const [comment, setComment] = useState({ username: user });
-  const [posted, setPosted] = useState(false);
+
   const handleChange = (e) => {
     setComment({ ...comment, [e.target.id]: e.target.value });
   };
@@ -15,8 +15,7 @@ const CommentForm = ({ review_id }) => {
     e.preventDefault();
     comment.body.length < 20
       ? alert("Comments must be at least 20 characters long!")
-      : postComment(review_id, comment);
-    setPosted(true);
+      : postComment(review_id, comment) && setPosted(true);
   };
   return (
     <form
--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,11 +1,20 @@
-import { useEffect, useState } from "react";
-import { fetchCommentsByReviewId } from "../api";
+import { useContext, useEffect, useState } from "react";
+import { deleteComment, fetchCommentsByReviewId } from "../api";
+import { IconButton } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 import CommentCard from "./CommentCard";
 import CommentForm from "./CommentForm";
+import { UserContext } from "../contexts/User";
 
 const Comments = ({ review_id }) => {
+  const { user } = useContext(UserContext);
   const [comments, setComments] = useState();
+  const [posted, setPosted] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
+  const handleClick = (comment_id) => {
+    deleteComment(comment_id);
+    setPosted(false);
+  };
   useEffect(() => {
     fetchCommentsByReviewId(review_id).then((comments) => {
       setComments(comments);
@@ -18,12 +27,29 @@ const Comments = ({ review_id }) => {
     <>
       {comments.length ? (
         comments.map((comment) => (
-          <CommentCard key={comment.comment_id} comment={comment} />
+          <>
+            <CommentCard key={comment.comment_id} comment={comment} />
+            {comment.author === user && (
+              <IconButton
+                aria-label="delete"
+                size="small"
+                onClick={() => {
+                  handleClick(comment.comment_id);
+                }}
+              >
+                <DeleteIcon fontSize="inherit" />
+              </IconButton>
+            )}
+          </>
         ))
       ) : (
         <p>No comments yet...</p>
       )}
-      <CommentForm review_id={review_id} />
+      <CommentForm
+        review_id={review_id}
+        posted={posted}
+        setPosted={setPosted}
+      />
     </>
   );
 };
